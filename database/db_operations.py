451554cc4ccf356f3e86f49488f804d98abd5162
EmmaMontalbanA <emonalv@gmail.com>
2024-10-24 12:29:29 +0200

    incluyo database en pruebas

---
+++ database/db_operations.py
@@ -0,0 +1,24 @@
+# database/db_operations.py
+from database.db_connection import get_db_connection
+
+def insert_prediction(data):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        sql_query = """
+            INSERT INTO predicciones (
+                gender, age, hypertension, heart_disease, avg_glucose_level,
+                smoking_status, prediction_result
+            )
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+        print("Consulta SQL:", sql_query)
+        print("Datos a insertar:", data)
+        cursor.execute(sql_query, data)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        print("Inserción exitosa en la base de datos")
+    except Exception as e:
+        print(f"Error en insert_prediction: {e}")
+
